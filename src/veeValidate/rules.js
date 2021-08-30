73aabe4679e652f84bc8d1e52d7ec7a8b374614e
dvasyliev <dvasyliev.dev@gmail.com>
2021-08-30 19:48:26 +0300

    Changed call types in validators

--- src/veeValidate/rules.js
+++ src/veeValidate/rules.js
@@ -2,35 +2,35 @@ import * as joiTypes from '../joi/types'
 import * as joiRules from '../joi/rules'
 
 const dateFormat = (type, value, options) => {
-  return joiRules['dateFormat'](joiTypes[type], options).validate(value)
+  return joiRules['dateFormat'](joiTypes[type](), options).validate(value)
 }
 
 const email = (type, value) => {
-  return joiRules['email'](joiTypes[type]).validate(value)
+  return joiRules['email'](joiTypes[type]()).validate(value)
 }
 
 const length = (type, value, options) => {
-  return joiRules['length'](joiTypes[type], options).validate(value)
+  return joiRules['length'](joiTypes[type](), options).validate(value)
 }
 
 const max = (type, value, options) => {
-  return joiRules['max'](joiTypes[type], options).validate(value)
+  return joiRules['max'](joiTypes[type](), options).validate(value)
 }
 
 const min = (type, value, options) => {
-  return joiRules['min'](joiTypes[type], options).validate(value)
+  return joiRules['min'](joiTypes[type](), options).validate(value)
 }
 
 const oneOf = (type, value, options) => {
-  return joiRules['oneOf'](joiTypes[type], options).validate(value)
+  return joiRules['oneOf'](joiTypes[type](), options).validate(value)
 }
 
 const phone = (type, value) => {
-  return joiRules['phone'](joiTypes[type]).validate(value)
+  return joiRules['phone'](joiTypes[type]()).validate(value)
 }
 
 const required = (type, value) => {
-  return joiRules['required'](joiTypes[type]).validate(value)
+  return joiRules['required'](joiTypes[type]()).validate(value)
 }
 
 export {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const path = require("path")
 
 module.exports = {
-  entry: path.resolve(__dirname,                                            "src/index.js"),
+  entry: path.resolve(__dirname, "src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    library: "$",
-    libraryTarget: "umd",
   },
   module: {
     rules: [
