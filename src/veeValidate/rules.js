2154d0a28c104c0321d681c863b20f7d65985abd
dvasyliev <dvasyliev.dev@gmail.com>
2021-09-09 19:02:01 +0300

    Fixed rules for vee validate

--- src/veeValidate/rules.js
+++ src/veeValidate/rules.js
@@ -4,49 +4,49 @@ import * as rules from '../joi/rules'
 
 const dateFormat = {
   validate: (value, options) => {
-    return !rules.dateFormat(types.date(), options)
+    return !rules.dateFormat(types.date(), options[0])
       .validate(value).error
   }
 }
 
 const length = {
   validate: (value, options) => {
-    return !rules.length(types.string(), options)
+    return !rules.length(types.string(), options[0])
       .validate(value).error
   }
 }
 
 const max = {
   validate: (value, options) => {
-    return !rules.max(types.string(), options)
+    return !rules.max(types.string(), options[0])
       .validate(value).error
   }
 }
 
 const maxValue = {
   validate: (value, options) => {
-    return !rules.max(types.number(), options)
+    return !rules.max(types.number(), options[0])
       .validate(value).error
   }
 }
 
 const min = {
   validate: (value, options) => {
-    return !rules.min(types.string(), options)
+    return !rules.min(types.string(), options[0])
       .validate(value).error
   }
 }
 
 const minValue = {
   validate: (value, options) => {
-    return !rules.min(types.number(), options)
+    return !rules.min(types.number(), options[0])
       .validate(value).error
   }
 }
 
 const oneOf = {
   validate: (value, options) => {
-    return !rules.oneOf(types.string(), options)
+    return !rules.oneOf(types.string(), options.join(','))
       .validate(value).error
   }
 }
