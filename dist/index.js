a72ccbb9e9b4081f2290c373b25e4173076ada65
Denys Koval <smithd.contact@gmail.com>
2021-09-20 14:24:17 +0300

    add config, add CI for PR

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
+const path = require('path')
+const config = require('../config')
+
 /**
  * Detect Electron renderer / nwjs process, which is node, but we should
  * treat as a browser.
  */
 
 if (typeof process === 'undefined' || process.type === 'renderer' || process.browser === true || process.__nwjs) {
-	module.exports = require('./client/index.js');
+	module.exports = require(path.resolve(__dirname, `${config.build.client.file}`));
 } else {
-	module.exports = require('./server/index.js');
+	module.exports = require(path.resolve(__dirname, `${config.build.server.file}`));
 }
